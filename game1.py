0aba1f61ec3b6839ce5938665a9c88472d2f8480
arvindsaik <krishnanarvind100@gmail.com>
2015-12-21 00:29:28 +0530

    Dynamic Obstacle Movement

--- game1.py
+++ game1.py
@@ -2,8 +2,8 @@ import pygame
 import time
 import random
 pygame.init()
-display_width = 800
-display_height = 600
+display_width = 1000
+display_height = 700
 black = (0,0,0)
 red = (255,0,0)
 white = (255,255,255)
@@ -15,6 +15,10 @@ clock = pygame.time.Clock()
 carImg = pygame.image.load('racecar.png')
 car_width=73
 
+#pygame.draw.circle(screen, color, (x,y), radius, thickness)
+def boost(boostx,boosty,radius):
+	pygame.draw.circle(gameDisplay, red, (boostx,boosty), 30, 1)
+	
 def  things_dodged(count):
 	font = pygame.font.SysFont(None,25)
 	text = font.render("Dodged: "+str(count), True,black)
@@ -52,12 +56,18 @@ def game_loop():
 	gameExit = False
 	thing_startx = random.randrange(0, display_width)
 	thing_starty = -600
-	thing_speed = 7
+	thing_speed_y = random.randrange(7,10)
+        thing_speed_x = (random.randrange(-1,2))*random.randrange(7,10)
+        if thing_speed_x > 0:
+        	f1=0
+        else:
+        	f1=1
+
 	thing_width = 100
 	thing_height = 100	
 	dodged = 0	
 	while not gameExit:
-	
+		
 		for event in pygame.event.get():
 		
 			if event.type == pygame.QUIT:
@@ -83,17 +93,30 @@ def game_loop():
 			thing_starty = 0 - thing_height
 			thing_startx = random.randrange(0, display_width)		
 			dodged +=1
-			thing_speed+=1
+			thing_speed_y = random.randrange(7,10)
+                	thing_speed_x = random.randrange(-1,2)*random.randrange(7,10)
+			if thing_speed_x > 0:
+				f1=0
+			else:
+				f1=1
+			
+			#thing_speed_y+=1
 
 		if y < thing_starty + thing_height:
 			print('y crossover')
 			if x > thing_startx and x < thing_startx + thing_width or x + car_width > thing_startx and x+ car_width<thing_startx+thing_width:
 				print('x crossover')
 				crash()
+		if thing_startx + thing_width >=  display_width and f1 == 0 :
+			thing_speed_x *= (-1)
+			f1=1
+		if thing_startx <= 0 and f1==1:
+			thing_speed_x *= (-1)
+			f1=0
 		gameDisplay.fill(white)
 		things(thing_startx, thing_starty, thing_width, thing_height, black)
-		thing_starty+=thing_speed
-
+		thing_starty+=thing_speed_y
+		thing_startx+=thing_speed_x
 		
 		car(x,y)
 		things_dodged(dodged)
